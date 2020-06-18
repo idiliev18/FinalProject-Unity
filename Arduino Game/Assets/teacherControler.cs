166d588d785c0af1e231a2ea655a5a50383467f1
Stoyan Ivanov <SSIvanov19@codingburgas.bg>
2020-06-18 21:52:46 +0300

    Add 3d objects and make AI for the teacher

---
+++ Arduino Game/Assets/teacherControler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+public class teacherControler : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 target;
+    public NavMeshAgent agent;
+
+    void Update()
+    {
+        target = player.position;
+
+        agent.SetDestination(target);
+    }
+}
