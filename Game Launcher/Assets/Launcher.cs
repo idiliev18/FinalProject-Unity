52404174c8d4ef42a8ac345fb619eb502fb0a834
DMKalchev18 <DMKalchev18@codingburgas.bg>
2020-06-19 22:17:44 +0300

    Create game launcher prototype

---
+++ Game Launcher/Assets/Launcher.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using System.Diagnostics;
+using System;
+using System.Configuration;
+
+public class Launcher : MonoBehaviour {
+    public string Directory;
+    public string URL;
+
+    public void Player_Game() {
+        Process.Start(Directory);
+    }
+
+    public void Quit_Launcher() {
+        Application.Quit();
+    }
+       
+    public void Open_Website() {
+        Application.OpenURL(URL);
+    }
+}
