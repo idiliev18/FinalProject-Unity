f64c17f6fb627d249e8c5729ef258a34d472a4ca
Stoyan Ivanov <SSIvanov19@codingburgas.bg>
2020-06-19 21:31:26 +0300

    Add basic UI

---
+++ Arduino Game/Assets/Scripts/buttonManagerLite.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class buttonManagerLite : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
