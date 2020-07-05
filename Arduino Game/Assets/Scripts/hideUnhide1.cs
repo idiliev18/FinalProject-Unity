b2c10081fd5a25a75bb057561ccb3801cf047b3d
Stoyan Ivanov <SSIvanov19@codingburgas.bg>
2020-07-06 00:10:24 +0300

    Reorganize folders

---
+++ Arduino Game/Assets/Scripts/hideUnhide1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations.Rigging;
+
+public class hideUnhide1 : MonoBehaviour
+{
+    public GameObject unhideObject;
+
+    public GameObject hint;
+
+    void OnMouseOver()
+    {
+        hint.SetActive(true);
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            unhideObject.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
+    
+    void OnMouseExit()
+    {
+        hint.SetActive(false);
+    }
+}
