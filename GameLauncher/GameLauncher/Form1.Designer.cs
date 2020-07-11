4a972e401aceb2ea458d6c63704a14b98aaa5c2b
IDIliev18 <IDIliev18@codingburgas.bg>
2020-07-11 16:47:27 +0300

    Remove maximize option

--- GameLauncher/GameLauncher/Form1.Designer.cs
+++ GameLauncher/GameLauncher/Form1.Designer.cs
@@ -207,11 +207,12 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(341, 472);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(343, 472);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.SignInPanel);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Team Byal Dunav Launcher";
             this.SignInPanel.ResumeLayout(false);
